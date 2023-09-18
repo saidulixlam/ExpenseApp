4fd47d684cd8533d4af14d6e6b4b5e14da6a7a09
saidulixlam <saidulixlam@gmail.com>
2023-09-18 13:51:33 +0530

    Delete expense added

--- src/components/Expenses/ExpenseDisplay.js
+++ src/components/Expenses/ExpenseDisplay.js
@@ -1,32 +1,61 @@
 import React from "react";
-import {  Card, ListGroup, Row, Col } from "react-bootstrap";
+import { Card, ListGroup, Row, Col, Button } from "react-bootstrap";
+// import { FaEdit, FaTrash } from "react-icons"; // Import Font Awesome icons
 
 const ExpenseDisplay = (props) => {
+
+  function editHandler(key) {
+    props.onEdit(key);
+  }
+
+  function deleteHandler(key) {
+    props.onDelete(key);
+  }
+
   return (
-    
-      <Row>
-        {props.expenses.map((item, index) => (
-          <Col key={index} md={4}>
-            <Card className="my-4 p-2 bg-light shadow rounded">
-              <Card.Body className="bg-light shadow">
-                <Card.Title>Expense Details</Card.Title>
-                <ListGroup variant="flush">
-                  <ListGroup.Item>
-                    <strong>Name:</strong> {item.description}
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <strong>Price:</strong> {item.price}
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <strong>Category:</strong> {item.category}
-                  </ListGroup.Item>
-                </ListGroup>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    )
+    <Row>
+      {props.expenses.map((item, index) => (
+       
+        <Col key={index} md={4}>
+          <Card className="my-4 p-2 bg-light shadow rounded">
+            <Card.Body className="bg-light shadow">
+              <Card.Title>Expense Details</Card.Title>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <strong>Name:</strong> {item.description}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <strong>Price:</strong> {item.price}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <strong>Category:</strong> {item.category}
+                </ListGroup.Item>
+              </ListGroup>
+              <div className="mt-3 d-flex justify-content-between">
+                {/* Edit Button */}
+                <Button 
+                variant="info" 
+                className="mr-3 btn-sm"
+                onClick={() => editHandler(item.key)}
+                >
+                  Edit
+                </Button>
+                {/* Delete Button */}
+                <Button 
+                variant="danger" 
+                className="mr-3 btn-sm"
+                onClick={() => deleteHandler(item.key)}
+                >
+                  Delete
+                </Button>
+              </div>
+
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 };
 
 export default ExpenseDisplay;
--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -9,12 +9,6 @@ const ExpenseForm = () => {
     const expenseCategoryRef = useRef(null);
 
     const [expenseItems, setExpenseItems] = useState([]);
-    // Declare formData state
-    const [formData, setFormData] = useState({
-        price: "",
-        description: "",
-        category: "food", // Set a default category if needed
-    });
 
     // State to control the visibility of the form
     const [showForm, setShowForm] = useState(false);
@@ -35,24 +29,32 @@ const ExpenseForm = () => {
         try {
             const res = await axios.post(`${firebaseURL}/expenses.json`, newExpense);
             if (res.status === 200 || res.status === 201) {
-                console.log(res.data);
+               
                 fetchExpense();
             } else {
                 throw new Error('Something went wrong');
             }
         } catch (err) {
-            console.error(err.message);
+            
             alert('Error submitting the form: ' + err.message);
         }
     };
 
+    
+
+
+    // Fetch expenses on component mount
     const fetchExpense = async () => {
         try {
-            const res = await axios.get(`${firebaseURL}/expenses.json`);
+            const res= await axios.get(`${firebaseURL}/expenses.json`);
             const expenseData = res.data;
             if (expenseData) {
-                // Convert the object of objects into an array
-                const expenseArray = Object.values(expenseData);
+                // Convert the object of objects into an array with keys
+                const expenseArray = Object.keys(expenseData).map(key => ({
+                    key, // Store the unique key of the expense object
+                    ...expenseData[key], // Include the expense object itself
+                }));
+               
                 setExpenseItems(expenseArray);
             } else {
                 console.log('No expense data found.');
@@ -61,16 +63,64 @@ const ExpenseForm = () => {
             console.error('Error fetching expenses: ' + err.message);
         }
     };
-
-    // Fetch expenses on component mount
-    useEffect(() => {
-        fetchExpense();
-    }, []);
-
+    
     // Handle toggle form visibility
     const handleAddExpenseClick = () => {
         setShowForm(!showForm);
     };
+
+     const editExpense = (key) => {
+    //     const expenseToEdit = expenseItems.find((expense) => expense.key === key);
+    //     setShowForm(true);
+    //     if (expenseToEdit) {
+    //         moneySpentRef.current.value = expenseToEdit.price;
+           
+    //         expenseDescriptionRef.current.value = expenseToEdit.description;
+    //         expenseCategoryRef.current.value = expenseToEdit.category;
+    //         // Show the form
+    //     }
+    // };
+    
+        // try {
+        //     const response = await axios.put(`${firebaseURL}/expenses/${key}.json`, updatedExpenseData);
+        //     if (response.status === 200 || response.status === 201) {
+                
+        //         // You can trigger a re-fetch or update your state as needed here.
+        //     } else {
+        //         throw new Error('Something went wrong while updating the expense.');
+        //     }
+        // } catch (error) {
+        //     console.error('Error editing expense:', error.message);
+        //     // Handle the error or display an alert as needed.
+         }
+     
+    
+
+    // Function to delete an expense
+    const deleteExpense = async (key) => {
+        console.log('i was click');
+        try {
+            const response = await axios.delete(`${firebaseURL}/expenses/${key}.json`);
+            if (response.status === 200) {
+                console.log('Expense deleted successfully:', response.data);
+                const newExpense=expenseItems.filter((item)=>{
+                   return item.key !==key;
+                })
+                setExpenseItems(newExpense);
+                // You can trigger a re-fetch or update your state as needed here.
+            } else {
+                throw new Error('Something went wrong while deleting the expense.');
+            }
+        } catch (error) {
+            console.error('Error deleting expense:', error.message);
+            // Handle the error or display an alert as needed.
+        }
+    };
+   
+    useEffect(() => {
+        fetchExpense();
+    }, []);
+
 
     return (
         <Container className="my-4 mx-sm-4 mx-md-5 p-4 rounded">
@@ -133,7 +183,8 @@ const ExpenseForm = () => {
                 </div>
             </Container>
             <Container className="bg-white p-4 rounded shadow-lg my-4 mx-4">
-                <ExpenseDisplay expenses={expenseItems} />
+               
+                <ExpenseDisplay expenses={expenseItems} onDelete={deleteExpense} onEdit={editExpense} />
             </Container>
         </Container>
     );
